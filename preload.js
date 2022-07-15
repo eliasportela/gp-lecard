15b1a065d7745ba32c7a2b640b21d43fa8e5856a
Elias Portela <edwportela@gmail.com>
2022-07-15 17:01:36 -0300

    v.1.7.1
    - Integracao ACBR

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const {ipcRenderer} = require('electron');
 const elStore = require("electron-store");
+const fs = require("fs");
 let store = new elStore();
 
 ipcRenderer.on('was-printed', (event, arg) => {
@@ -22,6 +23,46 @@ document.addEventListener("pauseNotification", () => {
   ipcRenderer.send('notification', false);
 }, false);
 
+document.addEventListener("comandoAcbr", (e) => {
+  // const comando = e.detail;
+  comandoACBR('BAL.LePeso');
+}, false);
+
 window.Electron = true;
 window.LecardCom = true;
 window.isComanda = !!store.get("isComanda");
+
+function comandoACBR(comando) {
+  fs.writeFile('C://ACBrMonitorPLUS/ent.txt', comando,null, function (err) {
+    if (err) throw err;
+
+    lerACBR(1, (res) => {
+      if (res) {
+        document.dispatchEvent(new CustomEvent('acbrReply', { detail: res }));
+      }
+    });
+  });
+}
+
+function lerACBR(cont, callback) {
+  console.log('lendo.. ' + cont)
+
+  if (fs.existsSync('C://ACBrMonitorPLUS/sai.txt')) {
+    fs.readFile("C://ACBrMonitorPLUS/sai.txt", 'utf-8', function (err, data) {
+      if(err) throw err;
+      callback(data);
+
+      try {
+        fs.unlinkSync('C://ACBrMonitorPLUS/sai.txt')
+
+      } catch(err) {
+        console.error(err)
+      }
+    });
+
+  } else if (cont < 10) {
+    setTimeout(() => {
+      lerACBR(++cont, callback);
+    }, 1000);
+  }
+}
