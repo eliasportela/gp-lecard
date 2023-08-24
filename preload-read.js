9055b8e94d9d60a1b928b7901529be9a02dfd5cf
Elias <edwportela@gmail.com>
2023-08-24 17:25:36 -0300

    edwp_remover_codigo

--- preload-read.js
+++ preload-read.js
@@ -9,10 +9,6 @@ document.addEventListener("print", (e) => {
   ipcRenderer.send('print', e.detail);
 }, false);
 
-document.addEventListener("openPDF", (e) => {
-  ipcRenderer.send('openPDF', e.detail);
-}, false);
-
 document.addEventListener("gopage", (e) => {
   ipcRenderer.send('gopage', e.detail);
 }, false);
